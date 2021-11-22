cbd9ec7ad8d2cc2bc7ad4cdfccb82a1c552de887
Sers <ryan.sers@dxc.com>
2021-11-22 11:21:42 +0000

    New theme, header and footer design & all js added

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,77 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
-
 const body = document.querySelector('body')
-
 const overlay = document.querySelector('.overlay');
-
 const fadeElems = document.querySelectorAll('.has-fade');
+
+const headerLogo = document.querySelector('content__Logo');
+
+//let scrollPos = window.scrollY
+
+const highlightMenu = () => {
+
+    const elem = document.querySelector('.section_seperator');
+    const aboutMenu = document.querySelector('#about-page');
+    const portfolioMenu = document.querySelector('#portf-page');
+    const ph_dMenu = document.querySelector('#ph_d-page');
+    let scrollPos = window.scrollY;
+
+
+    // Add 'highligh' class to my menu items
+
+    if(window.innerWidth > 960 && scrollPos < 600){
+
+        aboutMenu.classList.add('section_seperator');
+        portfolioMenu.classList.remove('section_seperator');
+        ph_dMenu.classList.remove('section_seperator');
+        return;
+
+    }else if (window.innerWidth > 960 && scrollPos < 1400){
+
+        aboutMenu.classList.remove('section_seperator');
+        portfolioMenu.classList.add('section_seperator');        
+        ph_dMenu.classList.remove('section_seperator');
+        return;
+
+    }else if (window.innerWidth > 960 && scrollPos < 2345){
+
+        ph_dMenu.classList.add('section_seperator');
+        portfolioMenu.classList.remove('section_seperator');
+        return;
+    }
+
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('section_seperator');
+      }
+};
+
+window.addEventListener('scroll', highlightMenu)
+window.addEventListener('click', highlightMenu)
+
+
+const gotoPortf = () => {
+
+    console.log('I was portf clicked');
+    window.scrollTo(0, 1400);
+
+    //console.log(window.scrollY);
+
+    //let scrollPos = window.scrollY;
+    //window.scrollY = 1400;
+
+ };
+
+ var gotoPh_d = () => {
+
+    console.log('I was clicked');
+   // let scrollPos = window.scrollY;
+  // scrollPos = 1400;
+
+ };
+
+ //const goPortf = document.querySelector('#portf-page').addEventListener('click', gotoPortf);
+ //const goPh_d = document.querySelector('#ph_d-page').addEventListener('click', gotoPh_d);
+ 
 
 btnHamburger.addEventListener('click', function(){
   console.log('click hamburger');
@@ -17,8 +83,6 @@ btnHamburger.addEventListener('click', function(){
         element.classList.remove('fade-in');  
         element.classList.add('fade-out'); 
     });
- 
-    
     
   }
   else { // Open Hamburger Menu
@@ -27,8 +91,11 @@ btnHamburger.addEventListener('click', function(){
     fadeElems.forEach(function(element){
         element.classList.remove('fade-out'); 
         element.classList.add('fade-in');
-    })
-
-     
+    })   
   }  
 });
+
+function sendMail() //Only wanted this to work with Outlook.
+{
+    window.location.href = "mailto:r.sers14@gmail.com?";
+}

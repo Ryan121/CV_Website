b2e57e58feca9af8799e3625403655690a86d764
Sers <ryan.sers@dxc.com>
2022-01-07 17:22:39 +0000

    v1 design near completion - intermediary commit

--- app/js/script.js
+++ app/js/script.js
@@ -128,6 +128,18 @@ function sendMail() //Only wanted this to work with Outlook.
     window.location.href = "mailto:r.sers14@gmail.com?";
 }
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
 
